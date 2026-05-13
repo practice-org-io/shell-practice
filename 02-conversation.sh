f115bbde83096620f3dbb970bf50d42e636b19b6
lokesh <lokesh.theofficial@gmail.com>
2026-05-13 13:08:45 +0530

    convo

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh, I'm fine . Thanks for asking. How are you doing?"
+echo "Suresh:: doing fine. What are you learning?"
+echo "Ramesh:: I'm learning shell . what about you?"
