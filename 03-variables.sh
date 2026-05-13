6bf31cbdcae3562cbae358dbc0109c7df79b8eba
lokesh <lokesh.theofficial@gmail.com>
2026-05-13 13:31:36 +0530

    some changes

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Suresh #no space
+PERSON2=Ramesh
+
+echo "$Person1:: Hello $Person2, How are you?"
+echo "$Person2:: Hi $Person1, I'm fine . Thanks for asking. How are you doing?"
+echo "$Person1:: doing fine. What are you learning?"
+echo "$Person2:: I'm learning shell . what about you?"
