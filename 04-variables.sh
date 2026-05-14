2bd244d265e604e5add8df4e7dad063eb953ff5f
lokesh <lokesh.theofficial@gmail.com>
2026-05-14 07:12:40 +0530

    some change

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Theodore #no space
-PERSON2=Snelton
+PERSON1=$1 #no space
+PERSON2=$2
 
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I'm fine . Thanks for asking. How are you doing?"
