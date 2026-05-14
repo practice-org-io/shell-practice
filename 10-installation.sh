3fb492b805d87359d7f23ada71000651803287bc
lokesh <lokesh.theofficial@gmail.com>
2026-05-14 11:20:36 +0530

    some change

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo "Trying to install Nginx"
+echo "Trying to install Nginx..."
 
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user access"
+    exit 1
 fi
 
 
