d6ffecb44c89ae4d82fa76abfd8ed98b94121497
lokesh <lokesh.theofficial@gmail.com>
2026-05-14 07:58:15 +0530

    some change

--- 07-special_vars.sh
+++ 07-special_vars.sh
@@ -2,3 +2,11 @@
 
 ### Special Variables ###
 echo "All args passed to script: $@"
+echo "Number of vars passed to script: $#"
+echo "Script name: $0"
+echo "Present directory: $PWD"
+echo "who is running: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID of this script: $$"
+sleep 10 &
+echo "PID of recently executed background process: $!"
