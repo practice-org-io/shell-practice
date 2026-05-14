2c0d0327009cb73bfc0c6ed865414312d27dd9a9
lokesh <lokesh.theofficial@gmail.com>
2026-05-14 10:46:55 +0530

    some change

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -11,5 +11,8 @@
 NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
-    echo "Given number: $NUMBER is greater than 25"
+    echo "Given number: $NUMBER is greater than 20"
+else
+    echo "Given number: $NUMBER is less than 20"
 fi
+
