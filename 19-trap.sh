cdcd22b10e7856e0984692e1088da42acdf485be
lokesh <lokesh.theofficial@gmail.com>
2026-05-15 09:34:07 +0530

    some change

--- 19-trap.sh
+++ 19-trap.sh
@@ -2,7 +2,10 @@
 
 set -e
 
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND" ' ERR
+
 echo "Hello World"
 echo "Iam learnings Shell"
 ech. "printing error here"
 echo "no error in this"
+
