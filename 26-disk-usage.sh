d0b4140e9350451610ea4f1ce8aa763457e92519
lokesh <lokesh.theofficial@gmail.com>
2026-05-20 12:41:02 +0530

    some change

--- 26-disk-usage.sh
+++ 26-disk-usage.sh
@@ -15,12 +15,12 @@ USAGE_THRESHOLD=3
 
 while IFS= read -r line
 do
-    USAGE=$(df -hT | grep -v Filesystem | awk '{print $6}' | cut -d "%" -f1)
-    PARTITION=$(df -hT | grep -v Filesystem | awk '{print $7}')
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
 
-    if [ $USAGE -gt $USAGE_THRESHOLD ]; then
-        MESSAGE+="High Disk usage on $PARTITION:$USAGE"
+    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+        MESSAGE+="High Disk usage on $PARTITION:$USAGE% \n"
     fi
 done <<< $DISK_USAGE
 
-echo "$MESSAGE"
+echo -e "$MESSAGE"
